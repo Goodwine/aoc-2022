b799153cc59b58c092046066ca9301630674458f
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2022-12-12 23:05:19 -0800

    day 13 - thanks to this talk I got some inspiration to parse the input https://youtu.be/x5p_SJNRB4U

---
+++ src/bin/day13.rs
@@ -0,0 +1,129 @@
+use std::{cmp::Ordering, str::FromStr, string::ParseError};
+
+use aoc;
+
+fn main() {
+  aoc::solve(
+    &|path| -> Vec<Packet> {
+      return aoc::lines(path)
+        .iter()
+        .filter(|line| !line.is_empty())
+        .map(|line| Packet::from_str(line.as_str()).unwrap())
+        .collect();
+    },
+    &p1,
+    &p2,
+  );
+}
+#[derive(Clone, Debug, PartialEq)]
+enum Packet {
+  Number(u8),
+  List(Vec<Packet>),
+}
+
+impl Packet {
+  fn parse(line: &[char]) -> (Self, usize) {
+    return match line[0] {
+      '[' => Self::parse_list(line),
+      '0'..='9' => Self::parse_number(line),
+      _ => panic!("impossible"),
+    };
+  }
+
+  fn parse_list(line: &[char]) -> (Self, usize) {
+    assert_eq!(line[0], '[');
+
+    let mut packets = vec![];
+
+    let mut i = 1;
+    loop {
+      match line[i] {
+        '[' | '0'..='9' => {
+          let (list, next_pos) = Self::parse(&line[i..]);
+          packets.push(list);
+          i += next_pos;
+        }
+        ']' => return (Self::List(packets), i + 1),
+        _ => i += 1, // ignore commas
+      }
+    }
+  }
+
+  fn parse_number(line: &[char]) -> (Self, usize) {
+    let number_raw = line
+      .iter()
+      .take_while(|c| match c {
+        '0'..='9' => true,
+        _ => false,
+      })
+      .collect::<String>();
+
+    return (Self::Number(number_raw.parse().unwrap()), number_raw.len());
+  }
+
+  /// Returns whether `self` is less than `other`. When they are equal it returns `None`.
+  fn less_than(&self, other: &Self) -> Option<bool> {
+    match (self, other) {
+      (Self::Number(a), Self::Number(b)) => {
+        if a == b {
+          None
+        } else {
+          Some(a < b)
+        }
+      }
+      (Self::List(a), Self::List(b)) => {
+        a.iter()
+          .zip(b)
+          .find_map(|(a, b)| a.less_than(b))
+          .or(if a.len() == b.len() {
+            None
+          } else {
+            Some(a.len() < b.len())
+          })
+      }
+      (Self::List(_), Self::Number(b)) => self.less_than(&Self::List(vec![Self::Number(*b)])),
+      (Self::Number(a), Self::List(_)) => Self::List(vec![Self::Number(*a)]).less_than(other),
+    }
+  }
+}
+
+impl FromStr for Packet {
+  type Err = ParseError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    return Ok(Self::parse(s.chars().collect::<Vec<char>>().as_slice()).0);
+  }
+}
+
+fn p1(data: Vec<Packet>) -> usize {
+  return data
+    .chunks(2)
+    .enumerate()
+    .filter(|(_, pair)| pair[0].less_than(&pair[1]).unwrap_or(true))
+    .map(|(index, _)| index + 1)
+    .sum();
+}
+
+fn p2(data: Vec<Packet>) -> usize {
+  let mut data = data.clone();
+
+  let p1 = Packet::from_str("[[2]]").unwrap();
+  let p2 = Packet::from_str("[[6]]").unwrap();
+
+  data.push(p1.clone());
+  data.push(p2.clone());
+
+  data.sort_by(|a, b| match a.less_than(b) {
+    Some(true) => Ordering::Less,
+    Some(false) => Ordering::Greater,
+    None => Ordering::Equal,
+  });
+
+  return data
+    .iter()
+    .enumerate()
+    .filter(|(_, packet)| **packet == p1 || **packet == p2)
+    .map(|(i, _)| i + 1)
+    .reduce(|acc, v| acc * v)
+    .unwrap();
+}
